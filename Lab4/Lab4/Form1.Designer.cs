80597363828a43948722b096e28b86630a617f4e
Вероника Вашуркина <67638959+VeronikaVashurkina@users.noreply.github.com>
2021-06-07 21:41:15 +0300

    Add files via upload

---
+++ Lab4/Lab4/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace Lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.LoadIm = new System.Windows.Forms.Button();
+            this.ImGray = new System.Windows.Forms.Button();
+            this.PrepIm = new System.Windows.Forms.Button();
+            this.FilterIm = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.оПрограммеToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(882, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(94, 20);
+            this.оПрограммеToolStripMenuItem.Text = "О программе";
+            this.оПрограммеToolStripMenuItem.Click += new System.EventHandler(this.оПрограммеToolStripMenuItem_Click);
+            // 
+            // LoadIm
+            // 
+            this.LoadIm.Location = new System.Drawing.Point(12, 37);
+            this.LoadIm.Name = "LoadIm";
+            this.LoadIm.Size = new System.Drawing.Size(102, 29);
+            this.LoadIm.TabIndex = 1;
+            this.LoadIm.Text = "Загрузка";
+            this.LoadIm.UseVisualStyleBackColor = true;
+            this.LoadIm.Click += new System.EventHandler(this.LoadIm_Click);
+            // 
+            // ImGray
+            // 
+            this.ImGray.Location = new System.Drawing.Point(12, 274);
+            this.ImGray.Name = "ImGray";
+            this.ImGray.Size = new System.Drawing.Size(102, 29);
+            this.ImGray.TabIndex = 2;
+            this.ImGray.Text = "Оттенки серого";
+            this.ImGray.UseVisualStyleBackColor = true;
+            this.ImGray.Click += new System.EventHandler(this.ImGray_Click);
+            // 
+            // PrepIm
+            // 
+            this.PrepIm.Location = new System.Drawing.Point(12, 309);
+            this.PrepIm.Name = "PrepIm";
+            this.PrepIm.Size = new System.Drawing.Size(102, 29);
+            this.PrepIm.TabIndex = 3;
+            this.PrepIm.Text = "Препарирование";
+            this.PrepIm.UseVisualStyleBackColor = true;
+            this.PrepIm.Click += new System.EventHandler(this.PrepIm_Click);
+            // 
+            // FilterIm
+            // 
+            this.FilterIm.Location = new System.Drawing.Point(12, 412);
+            this.FilterIm.Name = "FilterIm";
+            this.FilterIm.Size = new System.Drawing.Size(102, 39);
+            this.FilterIm.TabIndex = 4;
+            this.FilterIm.Text = "Матричные фильтры";
+            this.FilterIm.UseVisualStyleBackColor = true;
+            this.FilterIm.Click += new System.EventHandler(this.FilterIm_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(120, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(355, 266);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(506, 37);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(355, 266);
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(223, 306);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Изображение до обработки";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(160, 591);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(276, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Изображение после неполной пороговой обработки ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 369);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "0";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 344);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(102, 45);
+            this.trackBar1.TabIndex = 10;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(120, 322);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(355, 266);
+            this.pictureBox3.TabIndex = 11;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(506, 322);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(355, 266);
+            this.pictureBox4.TabIndex = 12;
+            this.pictureBox4.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(649, 306);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Оттенки серого";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(576, 591);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(218, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Изображение после матричного фильтра";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 29);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Сохранить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 108);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 16;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(29, 131);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(64, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Имя файла";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 157);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 29);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(882, 625);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.FilterIm);
+            this.Controls.Add(this.PrepIm);
+            this.Controls.Add(this.ImGray);
+            this.Controls.Add(this.LoadIm);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Обработка изображений";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.Button LoadIm;
+        private System.Windows.Forms.Button ImGray;
+        private System.Windows.Forms.Button PrepIm;
+        private System.Windows.Forms.Button FilterIm;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Lab4/Lab4/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            trackBar1.Minimum = 0;
+            trackBar1.Maximum = 255;
+            trackBar1.TickFrequency = 20;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void оПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Вариант 5, Препарирование изображения(неполная пороговая обработка), применение произвольных масочных фильтров");
+        }
+
+        private void LoadIm_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if (openFileDialog1.FileName == null) return;
+                pictureBox1.Load(openFileDialog1.FileName.ToString());
+
+            }
+        }
+
+        private void ImGray_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Image = null;
+            Bitmap img = new Bitmap(pictureBox1.Image,pictureBox1.Width,pictureBox1.Height);
+            Color col = new Color();
+            
+            for(int i=0; i<img.Width; i++)
+                for(int j=0; j<img.Height;j++)
+                {
+                    col = img.GetPixel(i, j);
+                    double gray = 0.3 * col.R + 0.59 * col.G + 0.11 * col.B;
+                    img.SetPixel(i, j, Color.FromArgb(Convert.ToInt32(gray), Convert.ToInt32(gray), Convert.ToInt32(gray)));
+                }
+            pictureBox2.Image = img;
+            col = img.GetPixel(200,200);
+        }
+
+        private void PrepIm_Click(object sender, EventArgs e)
+        {
+            if (pictureBox2.Image != null)
+            {
+                Bitmap bmp = new Bitmap(pictureBox2.Image, pictureBox2.Width, pictureBox2.Height);
+                int brg;
+                int choose = Int32.Parse(label3.Text.Trim());
+
+                for (int i = 0; i < bmp.Width; i++)
+                    for (int j = 0; j < bmp.Height; j++)
+                    {
+                        brg = (int)(bmp.GetPixel(i, j).R + bmp.GetPixel(i, j).G + bmp.GetPixel(i, j).B);
+                        if (brg >= choose)
+                        {
+                            brg = 255;
+                        }
+                        bmp.SetPixel(i, j, Color.FromArgb(brg, brg, brg));
+                    }
+                pictureBox3.Image = bmp;
+            }
+            else MessageBox.Show("Загрузите картинку");
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            label3.Text = trackBar1.Value.ToString();
+            PrepIm_Click(sender, e);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != string.Empty)
+                (pictureBox3.Image as Bitmap).Save(textBox1.Text + ".bmp", System.Drawing.Imaging.ImageFormat.Bmp);
+            else MessageBox.Show("Введите имя файла");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+            pictureBox2.Image = null;
+            pictureBox3.Image = null;
+            pictureBox4.Image = null;
+        }
+
+        private void FilterIm_Click(object sender, EventArgs e)
+        {
+            Filter(pictureBox4);
+        }
+        public void Filter(PictureBox pb)
+        {
+            int[,] matrix = new int[,] 
+            {{0, -1, 0},
+            {-1, 5, -1},
+            {0, -1, 0}};
+            Bitmap bmp = new Bitmap(pictureBox2.Image);
+            Bitmap bmp1 = new Bitmap(bmp.Width, bmp.Height);
+            int r, g, b;
+            for (int i = 2; i < bmp.Width - 2; i++)
+            {
+                for (int j = 2; j < bmp.Height - 2; j++)
+                {
+                    r = 0; g = 0; b = 0;
+                    for (int n = i - 2; n < i + 1; n++) 
+                    {
+                        for (int k = j - 2; k < j + 1; k++)
+                        {
+
+                            r += matrix[n + 2 - i, k + 2 - j] * bmp.GetPixel(n, k).R;
+                            g += matrix[n + 2 - i, k + 2 - j] * bmp.GetPixel(n, k).G;
+                            b += matrix[n + 2 - i, k + 2 - j] * bmp.GetPixel(n, k).B;
+                        }
+                    }
+                    bmp1.SetPixel(i, j, Color.FromArgb(Math.Max((Math.Min(r,255)),0), Math.Max((Math.Min(g, 255)), 0), Math.Max((Math.Min(b, 255)), 0)));
+                }
+            }
+
+            pb.Image = bmp1;
+        }
+    }
+}
